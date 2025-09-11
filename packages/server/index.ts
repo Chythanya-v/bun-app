02c80ee7241ec09038bc6ba03ce82420831450dd
Chythanya-v <chythanya.v27@gmail.com>
2025-09-12 05:20:20 +0530

    added backend

---
+++ packages/server/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import type { Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+const port = process.env.PORT || 4000
+
+
+
+app.get("/", (req: Request,res: Response)=> {
+res.send("Hello World!!")
+})
+
+app.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}`)
+})
