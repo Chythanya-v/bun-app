a3c01ea08dafa2b6c572a2f9f3c99a2ea5639c2e
Chythanya-v <chythanya.v27@gmail.com>
2025-09-12 05:48:30 +0530

    connected frontend to backend

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy : { // if we send any request wih /api it will get forwarded to host with port 4000
+      '/api' : 'http://localhost:4000'
+    }
+  }
 })
